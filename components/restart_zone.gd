aff94aff488c14e0c0b084ebb4d58005458f8df3
CristianOrmazabalC <c.ormazabal.1@ug.uchile.cl>
2024-11-01 21:29:23 -0300

    condicion de perdida para el jugador: tocar enemigo reinicia el nivel implementada usando restart_zone. Kill_zone, hitbox y hurtbox a medio implementar

---
+++ components/restart_zone.gd
@@ -0,0 +1,9 @@
+class_name RestartZone
+extends KillZone
+
+func _ready() -> void:
+	super._ready()
+
+func action(player: Player) -> void:
+	get_tree().reload_current_scene()
+	Gravity.set_gravity(Vector2.DOWN)
